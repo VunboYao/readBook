0564cd531876de564036a025d130a5cf060b46b3
VunboYao <vunbo@outlook.com>
2019-05-16 06:49:08 +0800

    douban

---
+++ DOUBAN/components/filmItem/filmItem.js
@@ -0,0 +1,29 @@
+// components/filmItem/filmItem.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  // options: {
+  //   styleIsolation: "apply-shared"
+  // },
+  properties: {
+      film: {
+        type: Object,
+        value: {}
+      }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- DOUBAN/pages/index/index.js
+++ DOUBAN/pages/index/index.js
@@ -1,3 +1,80 @@
 Page({
+    data: {
+        filmList: [
+            {
+                title: '',
+                films: {}
+            }
+        ]
+    },
+
+    onReady() {
+        wx.showLoading({
+            title: '加载中',
+        })
+        this.loadData()
+    },
+
+    /* 加载数据 */
+    loadData() {
+        wx.request({
+            url: 'https://m.douban.com/rexxar/api/v2/subject_collection/movie_free_stream/items',
+            data: {
+                start: 0,
+                count: 12
+            },
+            success: ({data: {subject_collection: {name}, subject_collection_items}}) => {
+                // 更新数据
+                this.setData({
+                    "filmList[1]": {
+                        title: name,
+                        films: subject_collection_items,
+                        reqKey: "movie_free_stream"
+                    }
+                })
+            }
+        })
+        wx.request({
+            url: "https://m.douban.com/rexxar/api/v2/subject_collection/movie_showing/items",
+            data: {
+                start: 0,
+                count: 12
+            },
+            success: ({data: {subject_collection: {name}, subject_collection_items}}) => {
+                // 更新数据
+                this.setData({
+                    "filmList[2]": {
+                        title: name,
+                        films: subject_collection_items,
+                        reqKey: "movie_showing"
+                    }
+                })
+                // 成功关闭loading
+                wx.hideLoading()
+            }
+        })
+        wx.request({
+            url: 'https://m.douban.com/rexxar/api/v2/subject_collection/movie_latest/items',
+            data: {
+                start: 0,
+                count: 12
+            },
+            success: ({data: {subject_collection: {name}, subject_collection_items}}) => {
+                // 更新数据
+                this.setData({
+                    "filmList[0]": {
+                        title: name,
+                        films: subject_collection_items,
+                        reqKey: "movie_latest"
+                    }
+                })
+            }
+        })
+    }
 
 })
+
+
+
+
+
