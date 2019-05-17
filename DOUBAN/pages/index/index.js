e33e169b39831eee5422776f4444621e382d4b7d
VunboYao <vunbo@outlook.com>
2019-05-17 20:14:24 +0800

    all ok

--- DOUBAN/pages/index/index.js
+++ DOUBAN/pages/index/index.js
@@ -1,78 +1,78 @@
-import api from "./../../api/api.js";
+import api from "./../../api/api.js"
 
 Page({
-    data: {
-        filmList: [
-            {
-                title: '',
-                films: {}
-            }
-        ]
-    },
+  data: {
+    filmList: [
+      {
+        title: '',
+        films: {}
+      }
+    ]
+  },
 
-    onReady() {
-        wx.showLoading({
-            title: '加载中',
-        })
-        this.loadData()
-    },
+  onReady() {
+    wx.showLoading({
+      title: '加载中',
+    })
+    this.loadData()
+  },
 
-    /* 加载数据 */
-    loadData() {
-        api.loadMovieShowing(({data: {subject_collection: {name}, subject_collection_items}}) => {
-            // 更新数据
-            this.setData({
-                "filmList[0]": {
-                    title: name,
-                    films: subject_collection_items,
-                    reqKey: "loadMovieShowing"
-                }
-            })
+  /* 加载数据 */
+  loadData() {
+    api.loadMovieShowing(({data: {subject_collection: {name}, subject_collection_items}}) => {
+      // 更新数据
+      this.setData({
+        "filmList[0]": {
+          title: name,
+          films: subject_collection_items,
+          reqKey: "loadMovieShowing"
+        }
+      })
 
-        })
-        api.loadMovieFreeStream(({data: {subject_collection: {name}, subject_collection_items}}) => {
-            // 更新数据
-            this.setData({
-                "filmList[1]": {
-                    title: name,
-                    films: subject_collection_items,
-                    reqKey: "loadMovieFreeStream"
-                }
-            })
-        })
-        api.loadMovieHot(({data: {subject_collection: {name}, subject_collection_items}}) => {
-            // 更新数据
-            this.setData({
-                "filmList[2]": {
-                    title: name,
-                    films: subject_collection_items,
-                    reqKey: "loadMovieHot"
-                }
-            })
-        })
-        api.loadMovieTop250(({data: {subject_collection: {name}, subject_collection_items}}) => {
-            // 更新数据
-            this.setData({
-                "filmList[3]": {
-                    title: name,
-                    films: subject_collection_items,
-                    reqKey: "loadMovieTop250"
-                }
-            })
-        })
-        api.loadMovieLatest(({data: {subject_collection: {name}, subject_collection_items}}) => {
-            // 更新数据
-            this.setData({
-                "filmList[4]": {
-                    title: name,
-                    films: subject_collection_items,
-                    reqKey: "loadMovieLatest"
-                }
-            })
-            // 关闭loading
-            wx.hideLoading();
-        })
-    }
+    })
+    api.loadMovieFreeStream(({data: {subject_collection: {name}, subject_collection_items}}) => {
+      // 更新数据
+      this.setData({
+        "filmList[1]": {
+          title: name,
+          films: subject_collection_items,
+          reqKey: "loadMovieFreeStream"
+        }
+      })
+    })
+    api.loadMovieHot(({data: {subject_collection: {name}, subject_collection_items}}) => {
+      // 更新数据
+      this.setData({
+        "filmList[2]": {
+          title: name,
+          films: subject_collection_items,
+          reqKey: "loadMovieHot"
+        }
+      })
+    })
+    api.loadMovieTop250(({data: {subject_collection: {name}, subject_collection_items}}) => {
+      // 更新数据
+      this.setData({
+        "filmList[3]": {
+          title: name,
+          films: subject_collection_items,
+          reqKey: "loadMovieTop250"
+        }
+      })
+    })
+    api.loadMovieLatest(({data: {subject_collection: {name}, subject_collection_items}}) => {
+      // 更新数据
+      this.setData({
+        "filmList[4]": {
+          title: name,
+          films: subject_collection_items,
+          reqKey: "loadMovieLatest"
+        }
+      })
+      // 关闭loading
+      wx.hideLoading()
+    })
+  }
 
 })
 
