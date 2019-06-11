84f4f32c2c3cdc9ec227de0787384b57dbc82ea2
VunboYao <vunbo@outlook.com>
2019-06-11 18:31:48 +0800

    OK

--- 小程序/Study/pages/index/index.js
+++ 小程序/Study/pages/index/index.js
@@ -10,6 +10,14 @@ Page({
     staffA: {firstName: 'Hulk', lastName: 'Hu'},
     staffB: {firstName: 'Shag', lastName: 'YOur'},
     staffC: {firstName: 'Giden', lastName: 'Line'},
+    items: [
+      { name: 'USA', value: '美国' },
+      { name: 'CHN', value: '中国', checked: 'true' },
+      { name: 'BRA', value: '巴西' },
+      { name: 'JPN', value: '日本' },
+      { name: 'ENG', value: '英国' },
+      { name: 'TUR', value: '法国' },
+    ]
   },
   hello() {
     common.sayHello('Vunbo');
@@ -19,5 +27,11 @@ Page({
   },
   bindViewTap(e) {
     console.log(e);
+  },
+  onDelete() {
+    this.data.items.splice(2,1);
+    this.setData({
+      items: this.data.items
+    })
   }
 })
