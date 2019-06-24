aa3dd787d2754ec52df84b03d623e7afdd6fda9a
VunboYao <vunbo@outlook.com>
2019-06-24 18:44:54 +0800

    vue

--- Node/000-write.js
+++ Node/000-write.js
@@ -20,3 +20,19 @@
     })
   }) */
 }
+{
+  const bar = () => console.log('bar');
+  const baz = () => console.log('baz');
+  const foo = () => {
+    console.log('foo');
+    setTimeout(() => {
+      bar()
+    }, 0);
+    new Promise((res, rej) => {
+      res('should be right after baz, before bar')
+    }).then(resolve => console.log(resolve))
+    baz()
+  }
+
+  foo()
+}
