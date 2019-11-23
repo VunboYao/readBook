bbbad320eb101b50955d887c03106bf68cb5e873
VunboYao <vunbo@outlook.com>
2019-11-23 13:01:05 +0800

    change

---
+++ package/vuex/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{count}}</h1>
+    <h2>{{$store.getters.doneTodosCount}}</h2>
+    <button @click="increment({amount: 10})">Add</button>
+  </div>
+</template>
+
+<script>
+	import {mapState, mapMutations} from 'vuex'
+
+	export default {
+		name: 'app',
+		data() {
+			return {
+				localCount: ''
+			}
+		},
+		computed: mapState({
+			count: state => state.count,
+			countAlias: 'count',
+			countPlusLocalState(state) {
+				return state.count + this.localCount
+			}
+		}),
+		methods: {
+			...mapMutations(['increment'])
+		}
+	}
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ package/vuex/src/store/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    count: 0,
+    countAlias: 'count',
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false }
+    ]
+  },
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    doneTodosCount: (state, getters) => {
+      return getters.doneTodos.length
+    }
+  },
+  mutations: {
+    increment(state, payload) {
+      state.count+=payload.amount
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
