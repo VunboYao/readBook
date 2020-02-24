45d824ee7dc3f3b86bd5c183ba95c28afc218648
VunboYao <vunboyao@MacBookPro.local>
2020-02-24 23:03:07 +0800

    apple 更新

--- music163/js/index.js
+++ music163/js/index.js
@@ -1,1 +1,18 @@
-let a = 1;
+const a = {
+    b: 2
+}
+Object.defineProperty(a, 'b', {
+    value: 3,
+    writable: true,
+    configurable: false,
+    enumerable: true
+})
+
+console.log(a.b);
+Object.defineProperty(a, 'b', {
+    value: 4,
+    writable: false,
+    configurable: false,
+    enumerable: true
+})
+console.log(a.b);
