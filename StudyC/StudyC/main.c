e727dfe771e42a90bb8b3319dbbeea2637209510
VunboYao <vunbo@outlook.com>
2020-03-10 09:54:45 +0800

    删除就文件，增加c

---
+++ StudyC/StudyC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StudyC
+//
+//  Created by VunboYao on 2020/3/7.
+//  Copyright © 2020 VunboYao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
