00e86a1d55a7fb1139ae5b7543d8129d86adbad0
VunboYao <vunbo@outlook.com>
2020-11-22 18:19:06 +0800

    some updated

--- StudyC/StudyC/main.c
+++ StudyC/StudyC/main.c
@@ -8,18 +8,11 @@
 
 #include <stdio.h>
 
-//int main(int argc, const char * argv[]) {
-//    // insert code here...
-//    printf("Hello, World!\n");
-//    return 0;
-//}
-int test(){
-    printf("ok");
+int main(){
+    int num = 12;
+    printf("Please input your name: \n");
+    scanf("%i", &num);
+    printf("num = %i\n", num);
     return 0;
 }
 
-int main(){
-    printf("姚远波第一\n");
-    test();
-    return 0;
-}
