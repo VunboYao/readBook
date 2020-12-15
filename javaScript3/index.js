cdf441c80a8654c2dc0aa6caddd09d0d152324b3
VunboYao <vunbo@outlook.com>
2020-12-15 23:46:01 +0800

    prototype

--- javaScript3/index.js
+++ javaScript3/index.js
@@ -1,3 +1,12 @@
-const arr = [1,2,3,4,5]
-const total = arr.reduce((prev,cur,index,arr) => prev + cur)
-console.log(total)
+const a = [1,-1,3,5,0]
+
+for (let i = 0; i < 5; i++) {
+	for (let j = 0; j < 5 - i; j++) {
+		if (a[j] > a[j + 1]) {
+			let temp = a[j]
+			a[j] = a[j + 1]
+			a[j + 1] = temp
+		}
+	}
+}
+console.log(a)
