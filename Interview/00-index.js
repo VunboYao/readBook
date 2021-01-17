fdd074d9462d7452f9120502bb2f232baf84a8fb
VunboYao <vunbo@outlook.com>
2021-01-18 00:19:57 +0800

    add:JS深浅拷贝

--- Interview/00-index.js
+++ Interview/00-index.js
@@ -87,7 +87,7 @@
 		})
 	}*/
 
-	let obj = {
+	/*let obj = {
 		value: 1,
 		valueOf() {
 			return 2
@@ -106,7 +106,53 @@
 	// {} 默认会调用 valueOf 是{},不是基础数据类型，调用toString, 返回"[object Object]"
 
 	console.log([1, 2, undefined, 4, 5] + 10) // 1,2,,4,510
-	// [1,2,undefined,4,5]会默认先调用valueOf结果还是这个数组，不是基础数据类型继续转换，也还是调用toString，返回"1,2,,4,5"，然后再和10进行运算
+	// [1,2,undefined,4,5]会默认先调用valueOf结果还是这个数组，不是基础数据类型继续转换，也还是调用toString，返回"1,2,,4,5"，然后再和10进行运算*/
+
+/*	let target = {}
+	let source = { a: { b: 1 }}
+	Object.assign(target, source)
+	console.log(target) // { a: { b: 1 } }
+	source.a.b = 10
+	console.log(source) // { a: { b: 10 } }
+	console.log(target) // { a: { b: 10 } }*/
+
+/*	let obj1 = {
+		a: {
+			b: 1
+		},
+		sym: Symbol(1)
+	}
+	Object.defineProperty(obj1, 'innumerable', {
+		value: '不可枚举属性',
+		enumerable: false
+	})
+	let obj2 = {}
+	Object.assign(obj2, obj1)
+	obj1.a.b = 2
+	console.log(obj1)
+	console.log(obj2)*/
+/*	let obj1 = {
+		a: 1,
+		b: {
+			c: 1
+		}
+	}
+	let obj2 = {...obj1}
+	obj2.a = 2
+	obj1.b.c = 2
+	console.log(obj1) // { a: 1, b: { c: 2 } }
+	console.log(obj2) // { a: 2, b: { c: 2 } }*/
+
+
+/*	let arr = [1,2,3]
+	let newArr = arr.concat()
+	newArr[1] = 100
+	console.log(arr) // [ 1, 2, 3 ]
+	console.log(newArr) // [ 1, 100, 3 ]*/
+	let arr = [1,2,{val:  4}]
+	let newArr = arr.slice()
+	newArr[2].val = 100
+	console.log(arr) // [ 1, 2, { val: 100 } ]
 }
 
 
