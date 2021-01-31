4ad80634c6b417595b8c9553fdc0059313f1b50b
VunboYao <vunbo@outlook.com>
2021-01-31 22:14:12 +0800

    feat:image-webpack-loader

--- WebPack/yyb.js
+++ WebPack/yyb.js
@@ -54,7 +54,30 @@ module.exports = {
 							name: '[name].[ext]',
 							outputPath: './img/' // 指定图片打包到特定的目录下
 						}
-					}
+					},
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								progressive: true,
+							},
+							// optipng.enabled: false will disable optipng
+							optipng: {
+								enabled: false,
+							},
+							pngquant: {
+								quality: [0.65, 0.90],
+								speed: 4
+							},
+							gifsicle: {
+								interlaced: false,
+							},
+							// the webp option will enable WEBP
+							webp: {
+								quality: 75
+							}
+						}
+					},
 				]
 			},
 			// CSS解析loader
