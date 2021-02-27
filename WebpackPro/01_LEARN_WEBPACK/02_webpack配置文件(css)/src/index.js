6abb251b6a1781042798b0cbe33aa617c7fd3516
VunboYao <vunbo@outlook.com>
2021-02-27 18:58:45 +0800

    webpack02

---
+++ WebpackPro/01_LEARN_WEBPACK/02_webpack配置文件(css)/src/index.js
@@ -0,0 +1,8 @@
+import { mul, add } from "./js/math.js";
+const {dateFormat, priceFormat} = require('./js/format.js')
+
+console.log(add(20, 30));
+console.log(mul(20, 30));
+
+console.log(dateFormat());
+console.log(priceFormat());
