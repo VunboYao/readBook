b7f16200878b26e586117e5f6570a4b7d98d7432
VunboYao <vunbo@outlook.com>
2021-05-06 10:52:14 +0800

    feat(react-router):路由的基本使用
    Route,Link,BrowserRouter

--- React/02-ReactCli/hello-react/src/App.jsx
+++ React/02-ReactCli/hello-react/src/App.jsx
@@ -1,9 +1,21 @@
 import {Component} from 'react'
-
+import {Link, Route} from 'react-router-dom'
+import About from './components/About'
+import Home from './components/Home'
+import './App.css'
 export default class App extends Component {
   render() {
     return (
-      <h2>Hello React</h2>
+      <div className='wrap'>
+          <div className='left'>
+            <Link to='/about'>关于</Link>
+            <Link to='/home'>主页</Link>
+          </div>
+          <div className='right'>
+            <Route path="/about" component={About} />
+            <Route path="/home" component={Home} />
+          </div>
+      </div>
     )
   }
 }
