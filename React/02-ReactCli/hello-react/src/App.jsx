d059e8f21f14942a17c2e223dce90755582c0187
VunboYao <vunbo@outlook.com>
2021-05-07 06:55:56 +0800

    feat(react): 路由复习

--- React/02-ReactCli/hello-react/src/App.jsx
+++ React/02-ReactCli/hello-react/src/App.jsx
@@ -1,22 +1,26 @@
-import { Component } from 'react'
-import { Route } from 'react-router-dom'
-import About from './pages/About'
-import Home from './pages/Home'
-import MyNavLink from './components/MyNavLink'
-import './App.css'
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Test from "./pages/Test";
+import MyNavLink from "./components/MyNavLink";
+import "./App.css";
 export default class App extends Component {
   render() {
     return (
-      <div className='wrap'>
-        <div className='left'>
-            <MyNavLink to="/about" children="About" />
-            <MyNavLink to="/home" children="Home" />
+      <div className="wrap">
+        <div className="left">
+          <MyNavLink to="/about" children="About" />
+          <MyNavLink to="/home" children="Home" />
         </div>
-        <div className='right'>
-          <Route path='/about' component={About} />
-          <Route path='/home' component={Home} />
+        <div className="right">
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route path="/home" component={Test} />
+            <Route path="/home" component={Home} />
+          </Switch>
         </div>
       </div>
-    )
+    );
   }
 }
