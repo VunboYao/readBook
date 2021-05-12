db98f3598b090c7042bb3a182197c9317beab88f
VunboYao <vunbo@outlook.com>
2021-05-12 11:49:44 +0800

    feat(react): react特性hooks学习

--- React/03-react_extension/01-setState/App.jsx
+++ React/03-react_extension/01-setState/App.jsx
@@ -1,3 +1,4 @@
+// 路由懒加载，引入lazy, Suspense
 import React, { Component } from 'react'
 import Demo from './components';
 export default class App extends Component {
--- React/03-react_extension/src/index.js
+++ React/03-react_extension/src/index.js
@@ -1,11 +1,4 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App/>, document.getElementById("root"))
