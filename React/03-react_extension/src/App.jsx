5cd26e790c68278c6ebaeb9631f3711bfce5055b
VunboYao <vunbo@outlook.com>
2021-05-28 09:00:25 +0800

    feat(react): 发布订阅模式实现方式

--- React/03-react_extension/src/App.jsx
+++ React/03-react_extension/src/App.jsx
@@ -1,26 +1,19 @@
 import React, { Component, Fragment } from 'react'
-import Demo from './components';
+import ComA from './components/ComponentA'
+import ComB from './components/ComponentB'
+
+import MyEventEmitter from './utils/pubSub'
+
+window.MyEvent = new MyEventEmitter()
 export default class App extends Component {
-  state = {
-    hasError: ''
-  }
+  state = {}
 
-  /* 捕获子组件生命周期错误，返回一个错误对象 */
-  static getDerivedStateFromError(err) {
-    return {
-      hasError: err
-    }
-  }
-
-  /* 捕获组件的错误，传递给后台 */
-  componentDidCatch(error, info) {
-    console.info(error, 'error');
-    console.info(info, 'info')
-  }
   render() {
     return (
       <Fragment>
-        {this.state.hasError ? 'sorry, there has some error' : <Demo />}
+        <h2>我是父组件</h2>
+        <ComA />
+        <ComB />
       </Fragment>
     )
   }
--- React/03-react_extension/src/components/ComponentA.jsx
+++ React/03-react_extension/src/components/ComponentA.jsx
@@ -2,17 +2,21 @@ import React, { PureComponent } from 'react'
 
 export default class index extends PureComponent {
   state = {
-    /* userList: [
+    infoToB: 'i am A info',
+    userList: [
       { id: '001', name: 'Yao' },
       { id: '002', name: 'Vunbo' },
       { id: '003', name: 'George' },
-    ], */
-    userList: 'demo'
+    ],
+  }
+  emitB = (params) => {
+    window.MyEvent.emit('someEvent', this.state.infoToB)
   }
   render() {
     return (
       <div>
-        <h2>i am ComA</h2>
+        <h3>i am component A</h3>
+        <button onClick={this.emitB}>触发B组件的更新数据</button>
         {this.state.userList.map(item => {
           return <p key={item.id}>{item.name}</p>
         })}
