dbe7516e813e11264dee6cbaf75a9a8eeb9249c8
VunboYao <vunbo@outlook.com>
2021-06-11 17:57:30 +0800

    feat(JS): JS基础右移操作

--- javaScript3/index.js
+++ javaScript3/index.js
@@ -1,3 +1,30 @@
-let a = new Object()
-let b = function (){}
-console.log(b.__proto__ === Function.prototype );
+{
+  let a = [1, 3, 6, 3, 23, 76, 1, 34, 222, 6, 456, 221]
+  function bubbleSort(array) {
+    const len = array.length
+    if (len < 2) return array
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < i; j++) {
+        if (array[j] > array[i]) {
+          const temp = array[j]
+          array[j] = array[i]
+          array[i] = temp
+        }
+      }
+    }
+    return array
+  }
+  bubbleSort(a)
+}
+
+{
+  let a = [1, 3, 6, 3, 23, 76, 1, 34, 222, 6, 456, 221]
+  function quickSort(array) {
+    let quick = function (arr) {
+      if (arr.length <= 1) return arr
+      const index = Math.floor(len >> 1)
+    }
+  }
+}
+
+
