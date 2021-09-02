289d074eaa0c2dfc2f4877814596ef315e9eebf5
VunboYao <vunbo@outlook.com>
2021-09-03 02:45:48 +0800

    Vue: vue-router手写mini版本

--- Vue/手写Vue/manul-vue/src/Krouter/vun-router.js
+++ Vue/手写Vue/manul-vue/src/Krouter/vun-router.js
@@ -1,60 +1,78 @@
-let Vue
+// 1.插件
+// 2.两个组件
+
+// vue插件：
+// function
+// 要求必须有一个install，将来会被Vue.use调用
+
+let Vue // 保存Vue构造函数，插件中要使用，不导入还能用
 class VunRouter{
 	constructor(options) {
 		// 保存选项
 		this.$options = options
 
-		const initial = window.location.hash.slice(1) || '/'
-		Vue.util.defineReactive(this, 'current', initial)
+    // 把current作为响应式数据
+    // 将来发生变化，router-view的render函数能够再次执行
+    // this指向路由实例
+    const initial = window.location.hash.slice(1) || '/'
+    Vue.util.defineReactive(this, 'current', initial)
 
-		window.addEventListener('hashchange', this.onhashchange.bind(this))
-	}
-	onhashchange() {
-		this.current = window.location.hash.slice(1)
-	}
+    // 监听hash变化
+    window.addEventListener('hashchange', () => {
+      this.current = window.location.hash.slice(1)
+    })
+  }
 }
 
+// 参数1是Vue.use调用时传入的
 VunRouter.install = function (_Vue) {
 	Vue = _Vue
 
-	/*挂载$router*/
-	Vue.mixin({
-		beforeCreate() {
-			// 只有根组件拥有router选项
-			if (this.$options.router) {
-				Vue.prototype.$router = this.$options.router
-			}
-		}
-	})
+  // 1. 挂载$router属性
+  // 全局混入目的：延迟下面逻辑到router创建完毕后并且附加到选项上时才执行
+  Vue.mixin({
+    beforeCreate() {
+      // 此钩子在每个组件创建实例时都会调用
+      // 根实例执行一次
+      if (this.$options.router) {
+        // this指向每一个Vue组件的实例
+        Vue.prototype.$router = this.$options.router
+      }
+    }
+  })
 
-	/*默认组件注册*/
-	Vue.component('router-link', {
-		props: {
-			to: {
-				type: String,
-				require: true
-			}
-		},
-		render(h) {
-			return h('a', {
-				attrs: {
-					href: `#${this.to}`
-				}
-			}, this.$slots.default)
-		}
-	})
+  // 2.router-link
+  Vue.component('router-link', {
+    props: {
+      to: {
+        type: String,
+        require: true
+      }
+    },
+    render(h) {
+      return h(
+        'a',
+        {
+          attrs: {
+            href: '#' + this.to
+          }
+        },
+        this.$slots.default
+      )
+    }
+  })
 
-	/*视图组件注册*/
-	Vue.component('router-view', {
-		render(h) {
-			let component = null
-			console.log(this.$router)
-			const route = this.$router.$options.routes.find(route => route.path === this.$router.current)
-			if (route) component = route.component
-			return h(component)
-		}
-	})
-
+  // 3.router-view
+  Vue.component('router-view', {
+    render(h) {
+      // this指向router-view的实例
+      console.log('this :>> ', this.$router.$options.routes);
+      let component = null
+      const route = this.$router.$options.routes.find( route => route.path === this.$router.current)
+      if (route) component = route.component
+      return h(component)
+    }
+  })
 }
 
 
