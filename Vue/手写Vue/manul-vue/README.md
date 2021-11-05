b0531da28198b05e90ba75cb5cd0dd9caddc276e
VunboYao <vunbo@outlook.com>
2021-11-05 13:58:51 +0800

    Vue-Router的实现

--- Vue/手写Vue/manul-vue/README.md
+++ Vue/手写Vue/manul-vue/README.md
@@ -1,24 +1,22 @@
-# manul-vue
+## vue-router 实现思路
 
-## Project setup
-```
-npm install
-```
+- 需要实现一个 install 方法。进行插件的注册
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+  - 默认传入的参数为 Vue 的构造函数
+  - 执行全局组件的注册：router-link 与 router-view
 
-### Compiles and minifies for production
-```
-npm run build
-```
+- router-link 通过 props 中传入的 to 属性，生成一个 a 标签
 
-### Lints and fixes files
-```
-npm run lint
-```
+  - render 方法通过 h(createElement)函数，生成虚拟 dom
+  - this.\$slots.default
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+- router-view
+
+  - 通过 hash 值监听当前的路由。匹配对应的组件
+  - 利用 Vue.mixin。在 beforeCreate 阶段，判断是否在 Vue 组件的根实例，包含 router 选项，则注册全局的\$router = this.options.router 获取到 VueRouter 的实例
+  - 通过 this.\$router.options.routes 去查找与 this.current 对应的路由对象
+  - 返回对应的 component
+
+- VueRouter 构造函数中保存对应的 options 选项。获取 routes
+  - 监听 hashchange，获取到当前的 hash 值。
+  - 利用 Vue.util.defineReactive 对 this.current 值进行响应式绑定
--- Vue/手写Vue/manul-vue/src/Krouter/vun-router.js
+++ Vue/手写Vue/manul-vue/src/Krouter/vun-router.js
@@ -6,10 +6,10 @@
 // 要求必须有一个install，将来会被Vue.use调用
 
 let Vue // 保存Vue构造函数，插件中要使用，不导入还能用
-class VunRouter{
-	constructor(options) {
-		// 保存选项
-		this.constructorOptions = options
+class VunRouter {
+  constructor(options) {
+    // 1.保存选项
+    this.constructorOptions = options
 
     // 把current作为响应式数据
     // 将来发生变化，router-view的render函数能够再次执行
@@ -22,7 +22,7 @@ class VunRouter{
     // match方法可以递归遍历路由表，获得匹配关系数组
     this.match()
 
-    // 监听hash变化
+    // 2.监听hash变化
     window.addEventListener('hashchange', () => {
       this.current = window.location.hash.slice(1)
       this.matched = []
@@ -51,21 +51,20 @@ class VunRouter{
 }
 
 // 参数1是Vue.use调用时传入的
-VunRouter.install = function (_Vue) {
-	Vue = _Vue
+VunRouter.install = function(_Vue) {
+  Vue = _Vue // 借鸡生蛋
 
   // 1. 挂载$router属性
   // 全局混入目的：延迟下面逻辑到router创建完毕后并且附加到选项上时才执行
   Vue.mixin({
     beforeCreate() {
       // 此钩子在每个组件创建实例时都会调用
-      // 根实例执行一次
+      // 根实例执行一次。生成路由实例
       if (this.$options.router) {
         // this指向每一个Vue组件的实例
-        // console.log('Vue组件的实例中的路由钩子 :>> ', this.$options.router);
         Vue.prototype.$router = this.$options.router
       }
-    }
+    },
   })
 
   // 2.router-link
@@ -73,20 +72,20 @@ VunRouter.install = function (_Vue) {
     props: {
       to: {
         type: String,
-        require: true
-      }
+        require: true,
+      },
     },
     render(h) {
       return h(
         'a',
         {
           attrs: {
-            href: '#' + this.to
-          }
+            href: '#' + this.to,
+          },
         },
         this.$slots.default
       )
-    }
+    },
   })
 
   // 3.router-view
@@ -96,7 +95,7 @@ VunRouter.install = function (_Vue) {
       this.$vnode.data.routerView = true
       let depth = 0
       let parent = this.$parent
-      while(parent) {
+      while (parent) {
         const vnodeData = parent.$vnode && parent.$vnode.data
         if (vnodeData) {
           if (vnodeData.routerView) {
@@ -107,8 +106,8 @@ VunRouter.install = function (_Vue) {
         parent = parent.$parent
       }
       // this指向router-view(Vue组件)的实例
-      // console.log('this.$router :>> ', this.$router);
-      // console.log('路由地址', this.$router.constructorOptions.routes);
+      console.log('this.$router 路由实例:>> ', this.$router)
+      console.log('路由地址列表', this.$router.constructorOptions.routes)
       let component = null
       // const route = this.$router.constructorOptions.routes.find( route => route.path === this.$router.current)
       // 从匹配数组中获取
@@ -116,9 +115,8 @@ VunRouter.install = function (_Vue) {
       const route = this.$router.matched[depth]
       if (route) component = route.component
       return h(component)
-    }
+    },
   })
 }
-
 
 export default VunRouter
