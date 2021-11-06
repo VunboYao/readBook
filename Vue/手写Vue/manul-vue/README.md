a45c8c4a03d0c8bf88e5fc5b286870b9a2a2f1ec
VunboYao <vunbo@outlook.com>
2021-11-06 14:06:07 +0800

    Vuex实现原理

--- Vue/手写Vue/manul-vue/README.md
+++ Vue/手写Vue/manul-vue/README.md
@@ -20,3 +20,45 @@
 - VueRouter 构造函数中保存对应的 options 选项。获取 routes
   - 监听 hashchange，获取到当前的 hash 值。
   - 利用 Vue.util.defineReactive 对 this.current 值进行响应式绑定
+
+## 项目管理
+
+- 坚实的基础
+  - 界定项目成员的职位和责任
+  - 了解客户的具体需求
+- 项目管理资源
+  - 确定项目需求
+  - 明确项目角色，合理分配资源
+- 项目里程碑
+  - 项目的生命周期。启动、计划、执行和结尾
+- 有效的沟通
+  - 相互了解，互相及时了解项目信息和变动情况
+- 核心优势
+  - 认清团队成员的优势所在。合理分配任务
+- 项目管理工具
+  - 记录报告、文件共享到输入信息、有效沟通到实现一致协作
+- 风险管理
+- 可交付的成果
+- 评估总结
+
+## Vuex 实现思路
+
+- 实现 Store 类
+
+  - 维护响应式 state,暴露 commit/dispatch
+  - install: 注册 store
+
+- install 中通过 mixin, 注册全局的\$store
+- 利用 new Vue 将 state 中的数据进行响应式处理
+  - 将 new Vue 中的 state 做处理：变为自有属性，不被代理
+  - 通过存取器 get/set 指定只能通过 state 来访问数据
+- commit
+  - 限制通过 mutations 中的方法来触发
+- dispatch
+  - 限制通过 actions 中的方法来触发。this 绑定问题，箭头函数/bind
+- getters
+  - 使用方式：可以通过\$store.getters.doubleState 来使用。因此向用户暴露一个 getters 对象
+  - 利用 new Vue 中的 computed 计算属性来实现数据的计算
+  - 从用户传入的 options 中取得 warpGetters。遍历获取用户定义的 getter
+  - computed 计算属性中的函数是无参数的；store 中 getters 里的函数是有参数 state 的。进行高阶函数封装，将 getters 中的方法转换为 computed 可以使用的无参数形式
+  - 为 getters 定义只读属性
--- Vue/手写Vue/manul-vue/src/Kstore/kvuex.js
+++ Vue/手写Vue/manul-vue/src/Kstore/kvuex.js
@@ -1,69 +1,92 @@
 let Vue
 
 class Store {
-	constructor(options) {
-		// 保存选项
-		this.mutations = options.mutations
-		this.actions = options.actions
+  constructor(options) {
+    // 1.保存选项
+    this.mutations = options.mutations
+    this.actions = options.actions
+    this._wrapGetters = options.getters
 
-		// state应该是响应式的
-		this._vm = new Vue({
-			data() {
-				return {
-					// 自有属性，希望$$state不被代理
-					$$state: options.state
-				}
-			},
-		})
+    // 定义computed选项
+    const computed = {}
+    // 向用户暴露一个getters
+    this.getters = {}
+    // {doubleCounter(state){}}
+    Object.keys(this._wrapGetters).forEach(key => {
+      // TODO:获取用户定义的getter
+      const fn = this._wrapGetters[key]
+      // 转换为computed可以使用的无参数形式
+      computed[key] = () => {
+        return fn(this.state)
+      }
+      // 为getters定义只读属性
+      Object.defineProperty(this.getters, key, {
+        get: () => {
+          return this._vm[key]
+        },
+      })
+    })
 
-		this.getters = {}
-		for (let k in options.getters) {
-			this.getters[k] = options.getters[k]
-			Object.defineProperty(this.getters, k, {
-				get() {
-					return options.getters[k](options.state)
-				}
-			})
-		}
-	}
+    // state应该是响应式的
+    this._vm = new Vue({
+      data() {
+        return {
+          // 自有属性，希望$$state不被代理
+          $$state: options.state,
+        }
+      },
+      computed,
+    })
 
-	// 存取器
-	get state() {
-		return this._vm._data.$$state
-	}
+    /* this.getters = {}
+    for (let k in options.getters) {
+      this.getters[k] = options.getters[k]
+      Object.defineProperty(this.getters, k, {
+        get() {
+          return options.getters[k](options.state)
+        },
+      })
+    } */
+  }
 
-	set state(v) {
-		console.error(v)
-	}
+  // 存取器
+  get state() {
+    return this._vm._data.$$state
+  }
 
-	// 箭头函数类型，this指向Store的实例
-	commit = (type, payload) => {
-		const entry = this.mutations[type]
-		if (!entry) {
-			return console.error('unknow mutation type')
-		}
-		entry(this.state, payload)
-	}
+  set state(v) {
+    console.error('禁止直接修改state')
+  }
 
-	dispatch = (type, payload) => {
-		const entry = this.actions[type]
-		if (!entry) {
-			return console.error('unknow dispatch type')
-		}
-		return entry(this, payload)
-	}
+  // 箭头函数类型，this指向Store的实例
+  commit = (type, payload) => {
+    const entry = this.mutations[type]
+    if (!entry) {
+      return console.error('unknow mutation type')
+    }
+    entry(this.state, payload)
+  }
+
+  dispatch = (type, payload) => {
+    const entry = this.actions[type]
+    if (!entry) {
+      return console.error('unknow dispatch type')
+    }
+    return entry(this, payload)
+  }
 }
 
 function install(_Vue) {
-	Vue = _Vue
-	// 注册全局的$store
-	Vue.mixin({
-		beforeCreate() {
-			if (this.$options.store) {
-				Vue.prototype.$store = this.$options.store
-			}
-		},
-	})
+  // 保存Vue构造函数
+  Vue = _Vue
+  // 注册全局的$store
+  Vue.mixin({
+    beforeCreate() {
+      if (this.$options.store) {
+        Vue.prototype.$store = this.$options.store
+      }
+    },
+  })
 }
 
-export default {Store, install}
+export default { Store, install }
