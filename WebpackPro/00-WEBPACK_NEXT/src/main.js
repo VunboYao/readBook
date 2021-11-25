708462ff36a3c682456c655b2771bd3998cabbd8
VunboYao <vunbo@outlook.com>
2021-11-25 17:37:03 +0800

    feat:webpack-02命令地址基于node_modules触发

--- WebpackPro/00-WEBPACK_NEXT/src/main.js
+++ WebpackPro/00-WEBPACK_NEXT/src/main.js
@@ -15,3 +15,8 @@ webpack-cli封装了对webpack的调用，类似一个服务调用管家
 * 3.默认执行webpack打包。需要有一个src文件，以及配置src/index.js《入口文件》
 * 4.可通过引入打包后的dist/main.js查看对应的结果
 * */
+
+/*
+* 1.相对路径问题：npx webpack --entry ./../src/main.js (解析路径问题)
+* 	.bin中请求路径基于node_modules触发
+* */
