e5149474cee7876a66fefce1895450446e6b9dd6
VunboYao <vunbo@outlook.com>
2021-11-27 14:27:45 +0800

    feat:webpack-02

--- WebpackPro/01-WEBPACK_BETTER/src/index.js
+++ WebpackPro/01-WEBPACK_BETTER/src/index.js
@@ -2,6 +2,13 @@ import bar from './js/bar.js'
 
 import add from './js/foo.js'
 
-import './index.css'
+import './style/index.css'
+import './style/less.less'
 
+function dom() {
+  const test = document.createElement('div')
+  test.innerHTML = 'Hello WEBPACK'
+  return test
+}
+document.body.appendChild(dom())
 console.log(add(4, 5))
--- WebpackPro/01-WEBPACK_BETTER/webpack.config.js
+++ WebpackPro/01-WEBPACK_BETTER/webpack.config.js
@@ -1,44 +1,53 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
-  mode: 'development',
-  // 入口
-  // 多页面应用，多入口 对应 多出口
-  entry: {
-    index: './src/index.js',
-    login: './src/login.js',
-  },
-  // 出口
-  output: {
-    // 生成资源存放的位置，必须是绝对路径
-    path: path.resolve(__dirname, './dist'),
-    // 生成资源的名称
-    filename: '[name]-new.js', // 占位符[name]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/public/login.html',
-      filename: 'login.html',
-      chunks: ['login'],
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/public/index.html',
-      filename: 'index.html',
-      chunks: ['index'],
-    }),
-  ],
+	mode: 'development',
+	// 入口
+	// 多页面应用，多入口 对应 多出口
+	entry: {
+		index: './src/index.js',
+		login: './src/login.js',
+	},
+	// 出口
+	output: {
+		// 生成资源存放的位置，必须是绝对路径
+		path: path.resolve(__dirname, './dist'),
+		// 生成资源的名称
+		filename: '[name]-new.js', // 占位符[name]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+			/* npm i less less-loader@7 -D */
+			{
+				test: /\.less$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					'less-loader'],
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/public/login.html',
+			filename: 'login.html',
+			chunks: ['login'],
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/public/index.html',
+			filename: 'index.html',
+			chunks: ['index'],
+		}),
+	],
 }
