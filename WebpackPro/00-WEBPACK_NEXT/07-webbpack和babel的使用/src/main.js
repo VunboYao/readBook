a42184e441050d9110755e952b7f62500404fdee
VunboYao <vunbo@outlook.com>
2021-12-03 13:35:35 +0800

    feat:webpack-next.babel

---
+++ WebpackPro/00-WEBPACK_NEXT/07-webbpack和babel的使用/src/main.js
@@ -0,0 +1,13 @@
+const foo = name => {
+	console.log(name)
+}
+
+foo('Yao')
+
+/*
+*
+* npm install @babel/preset-env -D
+*
+* npx babel src --out-dir dist --presets=@babel/preset-env
+*
+* */
---
+++ WebpackPro/00-WEBPACK_NEXT/07-webbpack和babel的使用/webpack.config.js
@@ -0,0 +1,35 @@
+const {resolve} = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {DefinePlugin} = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+module.exports = {
+	mode: 'development',
+	entry: './src/main.js',
+	devtool: 'source-map',
+	output: {
+		filename: 'bundle.js',
+		path: resolve(__dirname, './dist'), // 绝对路径。当前文件所在的绝对路径
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Yao',
+			filename: 'VunboYao.html',
+			template: './public/index.html'
+		}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: './public',
+					globOptions: {
+						ignore: ['**/index.html']
+					}
+				}
+			]
+		}),
+		new DefinePlugin({
+			BASE_URL: '"./"'
+		}),
+		new CleanWebpackPlugin()
+	]
+}
