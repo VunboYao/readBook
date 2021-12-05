2f5fb2eef6512452e2c79236dd7038a22bf71f3e
VunboYao <vunbo@outlook.com>
2021-12-06 00:35:50 +0800

    feat:webpack-next:eslint

---
+++ WebpackPro/00-WEBPACK_NEXT/08-webpack与typescript-eslint配置/src/react.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: 'Hello React'
+    }
+  }
+
+  render() {
+    return <div>{this.state.msg}</div>
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('api'))
