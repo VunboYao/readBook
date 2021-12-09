33910a02636d41e94635db53e45d6ff4278cb23b
VunboYao <vunbo@outlook.com>
2021-12-09 18:45:18 +0800

    feat:webpack-next:init optimize

---
+++ WebpackPro/00-WEBPACK_NEXT/013-webpack代码分离/config/webpack.config.common.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+const path = require('path')
+const resolveApp = require('./path')
+
+const config = {
+  entry: {
+    index: './src/index.js',
+    main: './src/main.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: resolveApp('./dist'), // TODO: 静态资源的输出目录。
+    // TODO: 默认空字符串。 访问时：host + publicPath + path
+    // 建议是：'/'. 本地访问则是：'./'
+    // publicPath: "",
+    clean: true,
+  },
+  resolve: {
+    modules: ['node_modules'], // 模块的解析目录
+    extensions: ['.ts', '.vue', '.jsx', '...'], // 可以使用 '...' 访问默认拓展名
+    mainFiles: ['index'], // 文件扩展名解析
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.js$/, // JS中用到JSX
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(), // vue编译插件
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      title: 'yao', // webpack-dev-server默认的模板文件名称必须是index.html
+    }),
+  ],
+}
+
+module.exports = config
