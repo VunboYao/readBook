cc5125107f543e81783a437b500ac7c7316eedc6
VunboYao <vunbo@outlook.com>
2021-12-27 02:26:35 +0800

    feat:JS-debounce

---
+++ 手写系列/003-防抖debounce&节流throttle.js
@@ -0,0 +1,50 @@
+/*
+* 防抖：避免频繁的触发某一个事件。等待时间范围内，当新的输入时，函数的触发会被频繁的推迟。
+*   搜索
+*   滚动
+*   缩放
+*   课堂问题拖堂
+*   回程技能
+* */
+
+/*
+* 节流：触发的事件很多。按照固定的频率触发。
+*   监听滚动
+*   鼠标移动
+*   频繁点击
+*   技能冷却
+* */
+
+// ？获取返回值：回调方法 || Promise
+function debounce(fn, delay = 0, immediate = false) {
+  // 1.定义一个定时器，保存上一次的定时器
+  let timer = null
+  let isInvoke = false
+  // 2.真正执行的函数
+  const _debounce =  function (...args) {
+    // 取消上一次的定时器
+    if (timer) clearTimeout(timer)
+
+    // 3.立即执行
+    if (immediate && !isInvoke) {
+      fn.apply(this, ...args)
+      isInvoke = true
+    } else {
+      timer = setTimeout(() => {
+        // 外部传入的函数
+        fn.apply(this, args)
+        isInvoke = false
+        timer = null
+      }, delay)
+    }
+  }
+
+  // 4.取消功能
+  _debounce.cancel = function () {
+    if (timer) clearTimeout(timer)
+    timer = null
+    isInvoke = false
+  }
+
+  return _debounce
+}
