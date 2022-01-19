4902310aa3307620d2bcbf01ae06ad2e8de0bc9b
VunboYao <vunbo@outlook.com>
2022-01-19 15:57:00 +0800

    fix(vue3): remove old project

--- Vue/street/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import ineechart from './ineechart/index'
-import commonTable from './commonTable/index'
-export default {
-    install(Vue) {
-        Vue.component('ineechart', ineechart);
-        Vue.component('commonTable', commonTable);
-    }
-}
