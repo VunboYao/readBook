03934219589b85e8fc39504d04645d49c9633880
Vunbo MacMini <vunbo@outlook.com>
2022-02-23 01:20:24 +0800

    feat(vite):文档复习

--- WebpackPro/00-WEBPACK_NEXT/27-vite/vite.config.js
+++ WebpackPro/00-WEBPACK_NEXT/27-vite/vite.config.js
@@ -1,5 +1,12 @@
 import Vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
+import { defineConfig } from 'vite'
 
-export default {
-  plugins: [Vue()]
-}
+export default defineConfig({
+  plugins: [
+    Vue(),
+    legacy({
+      targets: ['defaults', 'not ie 11']
+    })
+  ]
+})
