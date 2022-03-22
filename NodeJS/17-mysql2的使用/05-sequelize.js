3dfa5cdd82c8debf06804a34810013e5bc72cb45
VunboYao <vunbo@outlook.com>
2022-03-22 15:37:39 +0800

    feat(node): mysql在node中使用

---
+++ NodeJS/17-mysql2的使用/05-sequelize.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('test', 'root', 'root', {
+  port: 3306,
+  host: 'localhost',
+  dialect: 'mysql'
+})
+
+
+sequelize.authenticate().then(res => {
+  console.log("🚀 ~ file: 05-sequelize.js ~ line 11 ~ sequelize.authenticate ~ res", res)
+}).catch(err => {
+  console.log("🚀 ~ file: 05-sequelize.js ~ line 13 ~ sequelize.authenticate ~ err", err)
+})
