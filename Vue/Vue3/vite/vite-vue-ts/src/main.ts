0d709d0da3f22da81f3f1c3208075d33f1225a37
VunboYao <vunbo@outlook.com>
2022-03-29 14:02:14 +0800

    feat(vite):env配置

--- Vue/Vue3/vite/vite-vue-ts/src/main.ts
+++ Vue/Vue3/vite/vite-vue-ts/src/main.ts
@@ -2,3 +2,9 @@ import { createApp } from 'vue'
 import App from './App'
 
 createApp(App).mount('#app')
+
+// * 环境变量
+console.log(import.meta.env)
+
+// * 自定义的环境变量
+console.log(import.meta.env.VITE_TITLE)
