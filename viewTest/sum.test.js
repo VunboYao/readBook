e29eabe70fd5903ea62989fd2af7b97672a8a0b9
VunboYao <vunbo@outlook.com>
2022-04-26 17:32:43 +0800

    feat(jest): test 初探

---
+++ viewTest/sum.test.js
@@ -0,0 +1,46 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('null', () => {
+  const n = null
+  expect(n).toBeNull()
+  expect(n).toBeDefined()
+  expect(n).not.toBeUndefined()
+  expect(n).not.toBeTruthy()
+  expect(n).toBeFalsy()
+})
+
+test('zero', () => {
+  const z = 0
+  expect(z).not.toBeNull()
+  expect(z).toBeDefined()
+  expect(z).not.toBeUndefined()
+  expect(z).not.toBeTruthy()
+  expect(z).toBeFalsy()
+})
+test('two plus two', () => {
+  const value = 2 + 2
+  expect(value).toBeGreaterThan(3)
+  expect(value).toBeGreaterThanOrEqual(3.5)
+  expect(value).toBeLessThan(5)
+  expect(value).toBeLessThanOrEqual(4.5)
+
+  expect(value).toBe(4)
+  expect(value).toEqual(4)
+})
+
+test('两个浮点数之和', () => {
+  const value = 0.1 + 0.2
+  expect(value).toBeCloseTo(0.3)
+})
+
+test('there is no I in team', () => {
+  expect('team').not.toMatch(/I/)
+})
+
+test('but there is a "stop" in Christoph', () => {
+  expect('Christoph').toMatch(/stop/)
+})
