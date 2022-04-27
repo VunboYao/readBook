57a0d32c2dd250bce5ded12b31aa4706691ecd39
VunboYao <vunbo@outlook.com>
2022-04-27 09:05:59 +0800

    feat(test): testt demo

--- viewTest/sum.test.js
+++ viewTest/sum.test.js
@@ -44,3 +44,53 @@ test('there is no I in team', () => {
 test('but there is a "stop" in Christoph', () => {
   expect('Christoph').toMatch(/stop/)
 })
+
+const shoppingList = [
+  'diapers',
+  'kleenex',
+  'trash bags',
+  'paper towels',
+  'milk'
+]
+test('shoppingList includes milk', () => {
+  expect(shoppingList).toContain('milk')
+  expect(new Set(shoppingList)).toContain('milk')
+})
+
+function compileAndroidCode() {
+  throw new Error('you are using the wrong JDK')
+}
+test('compiling android goes as expected', () => {
+  expect(() => compileAndroidCode().toThrow())
+  expect(() => compileAndroidCode().toThrow(Error))
+
+  expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK')
+  expect(() => compileAndroidCode()).toThrow(/JDK/)
+})
+
+function fetchData() {
+  return new Promise(resolve => {
+    resolve('good')
+  })
+}
+test('the data is peaut butter', async () => {
+  /* return fetchData().then(data => {
+    expect(data).toBe('good')
+  }) */
+  const data = await fetchData()
+  expect(data).toBe('good')
+})
+test('the data is good', async () => {
+  await expect(fetchData()).resolves.toBe('good')
+})
+
+function forEach(items, callback) {
+  for (let index = 0; index < items.length; index++) {
+    callback(items[index])
+  }
+}
+
+const mockCallback = jest.fn(x => 42 + x)
+forEach([0, 1], mockCallback)
+
+expect(mockCallback.mock.calls.length).toBe(2)
