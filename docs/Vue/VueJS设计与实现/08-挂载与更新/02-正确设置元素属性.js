2d87072b615a043887b72486077c9e19b663a065
VunboYao <vunbo@outlook.com>
2022-05-06 09:08:51 +0800

    feat(mount): html properties 与 dom properties 处理

---
+++ docs/Vue/VueJS设计与实现/08-挂载与更新/02-正确设置元素属性.js
@@ -0,0 +1,469 @@
+
+const bucket = new WeakMap()
+const reactiveMap = new Map()
+const TriggerType = {
+  SET: 'SET',
+  ADD: 'ADD',
+  DELETE: 'DELETE'
+}
+const originMethod = Array.prototype.includes
+const arrayInstrumentations = {}
+!['includes', 'indexOf', 'lastIndexOf'].forEach(method => {
+  const originMethod = Array.prototype[method]
+  arrayInstrumentations[method] = function (...args) {
+    let res = originMethod.apply(this, args)
+
+    if (res === false) {
+      res = originMethod.apply(this.raw, args)
+    }
+    return res
+  }
+})
+let shouldTrack = true
+!['push', 'pop', 'shift', 'unshift', 'splice'].forEach(method => {
+  const originMethod = Array.prototype[method]
+  arrayInstrumentations[method] = function (...args) {
+    shouldTrack = false
+    let res = originMethod.apply(this, args)
+    shouldTrack = true
+    return res
+  }
+})
+
+const ITERATE_KEY = Symbol()
+function createReactive(obj, isShallow = false, isReadonly = false) {
+  return new Proxy(obj, {
+    get(target, key, receiver) {
+      if (key === 'raw') {
+        return target
+      }
+
+      if (Array.isArray(target) && arrayInstrumentations.hasOwnProperty(key)) {
+        return Reflect.get(arrayInstrumentations, key, receiver)
+      }
+
+      if (!isReadonly && typeof key !== 'symbol') {
+        track(target, key)
+      }
+      const res = Reflect.get(target, key, receiver)
+      if (isShallow) {
+        return res
+      }
+      if (typeof res === 'object' && res !== null) {
+        return isReadonly ? readonly(res) : reactive(res)
+      }
+      return res
+    },
+    set(target, key, newValue, receiver) {
+      if (isReadonly) {
+        console.warn(`属性${key}是只读的`)
+        return true
+      }
+      const oldValue = target[key]
+      const type = Array.isArray(target)
+        ? Number(key) < target.length ? TriggerType.SET : TriggerType.ADD
+        : Object.prototype.hasOwnProperty.call(target, key) ? TriggerType.SET : TriggerType.ADD
+
+      const res = Reflect.set(target, key, newValue, receiver)
+      if (target === receiver.raw) {
+        if (oldValue !== newValue && (oldValue === oldValue || newValue === newValue)) {
+          trigger(target, key, type, newValue)
+        }
+      }
+
+      return res
+    },
+    has(target, key) {
+      track(target, key)
+      return Reflect.has(target, key)
+    },
+    ownKeys(target) {
+      track(target, Array.isArray(target) ? 'length' : ITERATE_KEY)
+      return Reflect.ownKeys(target)
+    },
+    deleteProperty(target, key) {
+      if (isReadonly) {
+        console.warn(`属性${key}是只读的`)
+        return true
+      }
+      const hadKey = Object.prototype.hasOwnProperty.call(target, key)
+      const res = Reflect.deleteProperty(target, key)
+
+      if (res && hadKey) {
+        trigger(target, key, 'DELETE')
+      }
+      return res
+    }
+  })
+}
+
+function readonly(obj) {
+  return createReactive(obj, false, true)
+}
+
+function shallowReadonly(obj) {
+  // 浅只读
+  return createReactive(obj, true, true)
+}
+
+function reactive(obj) {
+  const existionProxy = reactiveMap.get(obj)
+  if (existionProxy) return existionProxy
+
+  // 否则，创建新的代理对象
+  const proxy = createReactive(obj)
+  // 存储到Map中，从而帮忙重复创建
+  reactiveMap.set(obj, proxy)
+  // !reactive 也具有自动脱 ref 的能力
+  return proxyRefs(proxy)
+}
+
+function shallowReactive(obj) {
+  return createReactive(obj, true)
+}
+
+function track(target, key) {
+  if (!activeEffect || !shouldTrack) return
+  let depsMap = bucket.get(target)
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()))
+  }
+  let deps = depsMap.get(key)
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()))
+  }
+  deps.add(activeEffect)
+  activeEffect.deps.push(deps)
+}
+
+function trigger(target, key, type, newValue) {
+  const depsMap = bucket.get(target)
+  if (!depsMap) return
+  const effects = depsMap.get(key)
+  const effectsToRun = new Set()
+
+  if (Array.isArray(target) && key === 'length') {
+    depsMap.forEach((effects, key) => {
+      if (key >= newValue) {
+        effects.forEach(effectFn => {
+          if (effectFn !== activeEffect) {
+            effectsToRun.add(effectFn)
+          }
+        })
+      }
+    })
+  }
+  if (type === TriggerType.ADD && Array.isArray(target)) {
+    const lengthEffects = depsMap.get('length')
+    lengthEffects && lengthEffects.forEach(effectFn => {
+      if (effectFn !== activeEffect) {
+        effectsToRun.add(effectFn)
+      }
+    })
+  }
+
+  effects && effects.forEach(effectFn => {
+    if (effectFn !== activeEffect) {
+      effectsToRun.add(effectFn)
+    }
+  })
+
+  if (type === TriggerType.ADD || type === TriggerType.DELETE) {
+    const iterateEffects = depsMap.get(ITERATE_KEY)
+    iterateEffects && iterateEffects.forEach(effectFn => {
+      if (effectFn !== activeEffect) {
+        effectsToRun.add(effectFn)
+      }
+    })
+  }
+
+  effectsToRun.forEach(effectFn => {
+    if (effectFn.options.scheduler) {
+      effectFn.options.scheduler(effectFn)
+    } else {
+      effectFn()
+    }
+  })
+}
+
+let activeEffect
+const effectStack = []
+
+function effect(fn, options = {}) {
+  const effectFn = () => {
+    cleanup(effectFn)
+    activeEffect = effectFn
+    effectStack.push(effectFn)
+    const res = fn()
+    effectStack.pop()
+    activeEffect = effectStack[effectStack.length - 1]
+    return res
+  }
+  effectFn.options = options
+  effectFn.deps = []
+  if (!options.lazy) {
+    effectFn()
+  }
+  return effectFn
+}
+
+function computed(getter) {
+  let value
+  let dirty = true
+
+  const effectFn = effect(getter, {
+    lazy: true,
+    scheduler() {
+      if (!dirty) {
+        dirty = true
+        trigger(obj, 'value')
+      }
+    }
+  })
+
+  const obj = {
+    get value() {
+      if (dirty) {
+        value = effectFn()
+        dirty = false
+      }
+      track(obj, 'value')
+      return value
+    }
+  }
+  return obj
+}
+
+function watch(source, cb, options) {
+  let getter
+  if (typeof source === 'function') {
+    getter = source
+  } else {
+    getter = () => traverse(source)
+  }
+
+  let oldValue, newValue
+
+  let cleanup
+  function onInvalidate(fn) {
+    cleanup = fn
+  }
+
+  const job = () => {
+    newValue = effectFn()
+    if (cleanup) {
+      cleanup()
+    }
+    cb(newValue, oldValue, onInvalidate)
+    oldValue = newValue
+  }
+
+  const effectFn = effect(
+    () => getter(),
+    {
+      lazy: true,
+      scheduler: () => {
+        if (options.flush === 'post') {
+          const p = Promise.resolve()
+          p.then(job)
+        } else {
+          job()
+        }
+      }
+    }
+  )
+  if (options.immediate) {
+    job()
+  } else {
+    oldValue = effectFn()
+  }
+}
+function traverse(value, seen = new Set()) {
+  if (typeof value !== 'object' || value === null || seen.has(value)) return
+  seen.add(value)
+  for (const k in value) {
+    traverse(value[k], seen)
+  }
+  return value
+}
+
+function cleanup(effectFn) {
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    const deps = effectFn.deps[i]
+    deps.delete(effectFn)
+  }
+  effectFn.deps.length = 0
+}
+
+const jobQueue = new Set()
+const p = Promise.resolve()
+let isFlushing = false
+function flushJob() {
+  if (isFlushing) return
+  isFlushing = true
+  p.then(() => {
+    jobQueue.forEach(job => job())
+  }).finally(() => {
+    isFlushing = false
+  })
+}
+
+function ref(val) {
+  const wrapper = {
+    value: val
+  }
+  Object.defineProperty(wrapper, '__v_isRef', {
+    value: true
+  })
+  console.log(reactive(wrapper));
+  return reactive(wrapper)
+}
+
+function toRef(obj, key) {
+  const wrapper = {
+    get value() {
+      return obj[key]
+    },
+    set value(val) {
+      obj[key] = val
+    }
+  }
+  Object.defineProperty(wrapper, '__v_isRef', {
+    value: true
+  })
+  return wrapper
+}
+
+function toRefs(obj) {
+  const ret = {}
+  for (const key in obj) {
+    ret[key] = toRef(obj, key)
+  }
+  return ret
+}
+
+function proxyRefs(target) {
+  return new Proxy(target, {
+    get(target, key, receiver) {
+      const value = Reflect.get(target, key, receiver)
+      return value.__v_isRef ? value.value : value
+    },
+    set(target, key, newValue, receiver) {
+      const value = target[key]
+      if (value.__v_isRef) {
+        value.value = newValue
+        return true
+      }
+      return Reflect.set(target, key, newValue, receiver)
+    }
+  })
+}
+
+
+function createRenderer(options) {
+  const {
+    createElement,
+    insert,
+    setElementText,
+    patchProps
+  } = options
+  function render(vnode, container) {
+    if (vnode) {
+      patch(container._vnode, vnode, container)
+    } else {
+      if (container._vnode) {
+        container.innerHTML = ''
+      }
+    }
+  }
+  function patch(n1, n2, container) {
+    // n1:旧 vnode
+    // n2: 新 vnode
+    // container: 容器
+    if (!n1) {
+      // !如果n1不存在，意味着挂载.则调用 mountElement 函数完成挂载
+      mountElement(n2, container)
+    } else {
+      // n1 存在，意味着打补丁
+    }
+  }
+
+  function mountElement(vnode, container) {
+    const el = createElement(vnode.type)
+
+    // todo:children
+    if (typeof vnode.children === 'string') {
+      setElementText(el, vnode.children)
+    } else if (Array.isArray(vnode.children)) {
+      vnode.children.forEach(child => {
+        patch(null, child, el)
+      })
+    }
+
+    // todo:props
+    if (vnode.props) {
+      for (const key in vnode.props) {
+        // 调用 patchProps 函数即可
+        patchProps(el, key, null, vnode.props[key])
+      }
+    }
+
+    insert(el, container)
+  }
+  return {
+    render
+  }
+}
+function shouldSetAsProps(el, key, value) {
+  // 特殊处理
+  if (key === 'form' && el.tagName === 'INPUT') return false
+  return key in el
+}
+// ! HTML Attributes 的作用是设置与之对应的DOM Properties 的初始值
+
+// =================================
+const renderer = createRenderer({
+  createElement(tag) {
+    return document.createElement(tag)
+  },
+  setElementText(el, text) {
+    el.textContent = text
+  },
+  insert(el, parent, anchor = null) {
+    parent.insertBefore(el, anchor)
+  },
+  patchProps(el, key, preValue, nextValue) {
+    if (shouldSetAsProps(el, key, nextValue)) {
+      // get typeof DOM Properties
+      const type = typeof el[key]
+      // ! 解决disabled的value为空字符串时（‘’）,也禁用
+      // if type === boolean, and value === '', then Correction true
+      if (type === 'boolean' && nextValue === '') {
+        el[key] = true
+      } else {
+        el[key] = nextValue
+      }
+    } else {
+      // if el don't have  DOM Properties, then use setAttribute setting
+      el.setAttribute(key, nextValue)
+    }
+  }
+})
+const vnode = {
+  type: 'h2',
+  props: {
+    id: 'id123',
+    age: '666'
+  },
+  children: [
+    {
+      type: 'p',
+      children: 'hello vue'
+    },
+    {
+      type: 'span',
+      children: 'Hello span'
+    }
+  ]
+}
+renderer.render(vnode, document.getElementById('root'))
--- docs/Vue/VueJS设计与实现/08-挂载与更新/index.js
+++
@@ -1,6 +0,0 @@
-function getValue() {
-  let el = document.querySelector('input')
-  console.log(el.value); // !获取当前值
-  console.log(el.getAttribute('value')); // !获取初始值
-  console.log(el.type); // !浏览器的默认校验为 text <input value="foo" />
-}
