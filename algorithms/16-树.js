2d0f6e0b5c4131e9cbe53aa254c05b7e6f63acc5
Vunbo MacMini <vunbo@outlook.com>
2022-06-12 15:43:03 +0800

    feat: 二叉树remove操作,前驱/后继

--- algorithms/16-树.js
+++ algorithms/16-树.js
@@ -123,9 +123,9 @@ class BinarySearchTree {
     if (node !== null) {
       // 1.拼接节点
       this.result += ` ${node.key}`
-      // 2. 处理所有经过的左子节点
+      // 2.处理所有经过的左子节点
       this.preOrderTraverseNode(node.left)
-      // 3. 处理所有经过的右子节点
+      // 3.处理所有经过的右子节点
       this.preOrderTraverseNode(node.right)
     }
   }
@@ -181,7 +181,99 @@ class BinarySearchTree {
   }
 
   remove(key) {
+    // 1.找到要删除的节点, or return false
+    // 1.1 定义变量
+    let current = this.root
+    let isLeftChild = true
+    let parent = null
 
+    // 1.2 开始寻找删除的节点
+    while (current.key !== key) {
+      parent = current
+      if (key < current.key) {
+        isLeftChild = true // 左节点
+        current = current.left
+      } else {
+        isLeftChild = false // 右节点
+        current = current.right
+      }
+
+      // 找到叶子节点，还是没有
+      if (current === null) return false
+    }
+
+    // 2.根据对应的情况删除节点
+    // 2.1.该节点是叶子节点（没有子节点）
+    if (current.left === null && current.right === null) {
+      if (current === this.root) { // 根节点
+        this.root = null
+      } else if (isLeftChild) { // 是左节点
+        parent.left = null
+      } else {
+        parent.right = null // 右节点
+      }
+    }
+    // 2.2.只有一个叶子节点
+    else if (current.right === null) {
+      if (current === this.root) {
+        this.root = current.left
+      } else if (isLeftChild) {
+        parent.left = current.left
+      } else {
+        parent.left = current.right
+      }
+    } else if (current.left === null) {
+      if (current === this.root) {
+        this.root = current.right
+      } else if (isLeftChild) {
+        parent.left = current.right
+      } else {
+        parent.right = current.right
+      }
+    }
+    // 2.3.有两个子节点
+    /*
+      !前驱&后继
+      在二叉搜索树中，比 current 小一点点的节点，称为 current 节点的前驱
+      比 current 大一点点的节点，称为 current 节点的后继
+    */
+    else {
+      // 1.获取后继节点
+      let successor = this.getSuccessor(current)
+
+      // 2.判断根节点
+      if (current === this.root) {
+        this.root = successor
+      } else if (isLeftChild) {
+        parent.left = successor
+      } else {
+        parent.right = successor
+      }
+
+      // 3.将删除节点的左子树 = current.left
+      successor.left = current.left
+    }
+  }
+  // 后继实现
+  getSuccessor(delNode) {
+    // 1.定义变量，保存找到的后继
+    let successor = delNode
+    let current = delNode.right
+    let successorParent = delNode
+
+    // 2.循环查找
+    while (current !== null) {
+      successorParent = successor // 后继父节点 = 上一个后继
+      successor = current
+      current = current.left // 右子树的左子节点，后继
+    }
+
+    // 3.判断寻找到的后继节点是否直接是delNode的right节点
+    if (successor !== delNode.right) {
+      successorParent.left = successor.right // !顺序在前
+      successor.right = delNode.right
+    }
+    return successor
   }
 }
 
@@ -226,3 +318,9 @@ console.log(`min: ${min}, max: ${max}`);
 // *7查询
 let search = bst.search(25)
 console.log(`search: ${search}`);
+
+// *8删除
+bst.remove(9)
+bst.remove(7)
+bst.remove(15)
+console.log(`remove: ${bst.postOrderTraverse()}`);
