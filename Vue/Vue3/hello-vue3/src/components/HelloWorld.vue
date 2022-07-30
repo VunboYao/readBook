001217010003f8375812ae0af6f2a9271c75e80c
VunboYao <vunbo@outlook.com>
2022-07-30 18:50:28 +0800

    feat: demo

--- Vue/Vue3/hello-vue3/src/components/HelloWorld.vue
+++ Vue/Vue3/hello-vue3/src/components/HelloWorld.vue
@@ -33,7 +33,7 @@ export default {
     },
     titleModifiers: { // arg + 'Modifiers'
       type: Function,
-      default: () => null,
+      default: () => ({}),
     },
     modelModifiers: { // 默认修饰符
       type: Object,
--- Vue/Vue3/hello-vue3/src/components/ts-setup.vue
+++ Vue/Vue3/hello-vue3/src/components/ts-setup.vue
@@ -6,7 +6,14 @@
 </template>
 
 <script setup lang="ts">
+import { defineProps } from "vue"
+
 let count: string | number = 1
+interface Props {
+  foo: string
+  bar?: number
+}
+const { foo, bar = 100 } = defineProps<Props>()
 </script>
 
 <style scoped>
