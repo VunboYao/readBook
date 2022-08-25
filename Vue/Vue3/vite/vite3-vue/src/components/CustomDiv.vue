9a17bf943ef8f52694b847e8ae86ed0851359ba3
yaoyuanbo <yaoyuanbo@xurongwangluo.com>
2022-08-25 11:57:20 +0800

    feat: Vue自定义指令限频方案

---
+++ Vue/Vue3/vite/vite3-vue/src/components/CustomDiv.vue
@@ -0,0 +1,22 @@
+<template>
+<div v-bind="$attrs" @click.self="myClick"><slot></slot></div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const emit = defineEmits(['myClick'])
+let flag = ref(false)
+const myClick = (el) => {
+  if (!flag.value) {
+    flag.value = true
+    emit('myClick', 'surprize')
+    setTimeout(() => {
+      flag.value = false
+    }, 2000)
+  }
+}
+</script>
+
+<style scoped>
+</style>
