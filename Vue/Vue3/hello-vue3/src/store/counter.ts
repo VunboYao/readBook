685f31dca853d792b84a2026090a9ce469041658
VunboYao <vunbo@outlook.com>
2022-08-28 16:43:04 +0800

    feat: new Pinia store

---
+++ Vue/Vue3/hello-vue3/src/store/counter.ts
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia"
+
+export const useCountStore = defineStore({
+  id: 'count',
+  state: () => ({
+    number: 2,
+  }),
+  getters: {
+    doubleNumber(state) {
+      return state.number * 2
+    },
+  },
+  actions: {
+    add() {
+      this.number++
+      console.log(this.number)
+    },
+  },
+})
