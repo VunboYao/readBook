d53b78733ce637d2fa8a1c8ea9cb5e669701bb54
VunboYao <vunbo@outlook.com>
2022-11-19 10:23:00 +0800

    docs: comment更新

--- docs/Vue/VueJS设计与实现/02-框架设计的核心要素/index.js
+++ docs/Vue/VueJS设计与实现/02-框架设计的核心要素/index.js
@@ -2,10 +2,12 @@
 输出格式：
 html中使用: IIFE(immediately invoked Function Expression) vue.global.js
 浏览器emModule使用: vue.esm-browser.js
-webpack等模块化中使用时：优先选择package.json中的module，而不是main，vue.esm-bundler.js
+webpack等模块化中使用时：优先选择package.json中的module，而不是main，使用vue.esm-bundler.js
 服务端渲染：vue.cjs.js
 
 app.config.errorHandler: 注册错误处理程序
+
+Vue3中无副作用的函数标识，告知rollup打包时，此代码若未使用，可以删除
 */
 
 let handleError = null
