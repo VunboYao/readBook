8f6a349b94133c6e8e5c8cd0a2e2ed33a5bd9c5d
yaoyuanbo <yaoyuanbo@xurongwangluo.com>
2023-01-03 10:44:10 +0800

    feat:跨越请求头相关权限设置

--- NodeJS/11-HTTP/09-response-响应header.js
+++ NodeJS/11-HTTP/09-response-响应header.js
@@ -2,16 +2,22 @@ const Http = require('http')
 
 // create server
 const server = Http.createServer((req, res) => {
-  console.log('req.message', req.headers)
+  console.log(req.headers.origin,'>>>')
   // TODO: 1.设置返回header
   // res.setHeader('Content-Type', 'application/json;charset=utf8')
   // TODO: 2.writeHead一起写入
   res.writeHead(200, {
     'Content-Type': 'text/html;charset=utf8',
-    "access-control-allow-origin": "*",
-    "access-control-allow-headers": "*",
+    // 允许的跨域请求站点，等同于*
+    "access-control-allow-origin": req.headers.origin,
+    // 允许客户端请求时，携带cookie
+    "access-control-allow-Credentials": true,
+    // 允许客户端请求时，携带的请求头
+    "access-control-allow-headers": "X-Test-Cors",
     "access-control-expose-headers": "x-demo-xxx,xixi-demo,X-demo-1",
     "X-demo-xxx": "123",
+    // 设置cookie
+    "Set-Cookie": ['name=vunbo','age=26'],
     "xixi-demo": 'xixi',
     "X-demo-1": "demo"
   })
@@ -19,7 +25,7 @@ const server = Http.createServer((req, res) => {
   res.end("<h2>Hello Server</h2>") // 相当于res.write 和 res.close
 })
 
-server.listen(9999, 'localhost', () => {
+server.listen(9999, () => {
   console.log('server listening in port 9999', `http://localhost:9999`);
 })
 
