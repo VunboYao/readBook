087240f08f7983ce47799b49c363ca7e0ccf2e4f
VunboYao <vunbo@outlook.com>
2023-03-24 17:32:58 +0800

    fix: 修复节流处理返回函数，同步处理导致的问题

--- 手写系列/004-节流/004-02.js
+++ 手写系列/004-节流/004-02.js
@@ -1,16 +1,16 @@
-/* 
+/*
 leading: 第一次是否立即执行
 trailing: 最后一次是否执行
 */
 function throttle(fn, wait = 200, options = { leading: true, trailing: false }) {
   const { leading, trailing } = options
-  
+
   // 事件触发的时间
   let start_time = 0
   // 上一次事件执行的时间
   let last_time = 0
   let timer
-  
+
   function _throttle(...args) {
     // 定时器清理
     if (timer) {
@@ -21,11 +21,11 @@ function throttle(fn, wait = 200, options = { leading: true, trailing: false }) 
     // 事件触发的时间
     start_time = new Date().getTime()
 
-    return new Promise(async (resolve, reject) => {
+    return new Promise( (resolve, reject) => {
       // !没有上一次的执行时间，第一次是否根据配置立即执行
       if (!last_time && leading) {
         try {
-          const result = await fn.apply(this, args)
+          const result =  fn.apply(this, args)
           resolve(result)
           // 更新上一次事件的执行时间
           last_time = start_time
@@ -40,7 +40,7 @@ function throttle(fn, wait = 200, options = { leading: true, trailing: false }) 
 
           // 超出时间限制，可以执行
           if (duration >= wait) {
-            const result = await fn.apply(this, args)
+            const result = fn.apply(this, args)
             resolve(result)
             // 更新上一次事件的执行时间
             last_time = start_time
@@ -48,7 +48,7 @@ function throttle(fn, wait = 200, options = { leading: true, trailing: false }) 
             // !如果最后一次需要执行
             if (trailing) {
               // 更改定时器，延迟执行
-              timer = setTimeout(async () => {
+              timer = setTimeout( () => {
                 const result = fn.apply(this, args)
                 resolve(result)
 
