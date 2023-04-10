f2f2412b945363992824c19c7604044324c2b242
VunboYao <vunbo@outlook.com>
2023-04-10 20:31:20 +0800

    feat: change filename

--- 小程序/music/utils/query-select.js
+++
@@ -1,9 +0,0 @@
-export default function querySelect(selector) {
-  return new Promise(resolve => {
-    // 返回一个对象实例
-    const query = wx.createSelectorQuery()
-    query.select(selector).boundingClientRect(res => {
-      resolve(res)
-    }).exec()
-  })
-}
