db6c5c6d6b35ef4f4d5f6769f844b646f83a3d04
VunboYao <yaoyuanbo@lansijie.com>
2024-03-17 17:26:13 +0800

    feat: 算法相关

--- algorithms/TS/01-Stack/02-decimalToBinary.ts
+++ algorithms/TS/01-Stack/02-decimalToBinary.ts
@@ -2,26 +2,26 @@ import { ArrayStack } from "./01-index";
 
 
 function decimalToBinary(decimal: number): string {
-	// 1.创建一个栈来存储余数
-	const stack = new ArrayStack<number>()
+  // 1.创建一个栈来存储余数
+  const stack = new ArrayStack<number>()
 
-	// 2.循环：
-	// while: 不确定次数，只知道循环结束的条件
-	// for：需要知道循环的次数
-	while (decimal > 0) {
-		const remainder = decimal % 2
-		stack.push(remainder)
+  // 2.循环：
+  // while: 不确定次数，只知道循环结束的条件
+  // for：需要知道循环的次数
+  while (decimal > 0) {
+    const remainder = decimal % 2
+    stack.push(remainder)
 
-		decimal = Math.floor(decimal / 2)
-	}
+    decimal = Math.floor(decimal / 2)
+  }
 
-	// 3.已经获得所有的余数，遍历从栈顶取出
-	let binary = ''
-	while (!stack.isEmpty()) {
-		binary += stack.pop()
-	}
+  // 3.已经获得所有的余数，遍历从栈顶取出
+  let binary = ''
+  while (!stack.isEmpty()) {
+    binary += stack.pop()
+  }
 
-	return binary
+  return binary
 }
 
 
--- algorithms/TS/01-Stack/03-有效的括号.ts
+++ algorithms/TS/01-Stack/03-有效的括号.ts
@@ -1,38 +1,38 @@
 import { ArrayStack } from "./01-index";
 
 function isValid(s: string): boolean {
-	// 1.首先创建一个栈
-	const stack = new ArrayStack<string>()
+  // 1.首先创建一个栈
+  const stack = new ArrayStack<string>()
 
-	// 2.遍历传入的符号
-	for (let i = 0; i < s.length; i++) {
-		const t = s[i]
+  // 2.遍历传入的符号
+  for (let i = 0; i < s.length; i++) {
+    const t = s[i]
 
-		// 如果匹配对应的符号，则把相应的符号push到栈中
-		switch (t) {
-			case '(':
-				stack.push(')')
-				break
-			case '[':
-				stack.push(']')
-				break
-			case '{':
-				stack.push('}')
-				break
-			default:
-				// 如果出栈的和当前的不匹配，则false
-				if (t !== stack.pop()) {
-					return false
-				}
-		}
-	}
+    // 如果匹配对应的符号，则把相应的符号push到栈中
+    switch (t) {
+      case '(':
+        stack.push(')')
+        break
+      case '[':
+        stack.push(']')
+        break
+      case '{':
+        stack.push('}')
+        break
+      default:
+        // 如果出栈的和当前的不匹配，则false
+        if (t !== stack.pop()) {
+          return false
+        }
+    }
+  }
 
-	// 3.最终返回是否为空，不是空则无效
-	return stack.isEmpty()
+  // 3.最终返回是否为空，不是空则无效
+  return stack.isEmpty()
 }
 
 console.log(isValid("()"))
 console.log(isValid("([]){}"))
 console.log(isValid("(]"))
 
-export {}
+export { }
