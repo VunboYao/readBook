79c15b17f8d1896aa8f377ee1e74754613e04d50
VunboYao <yaoyuanbo@lansijie.com>
2024-10-05 00:08:45 +0800

    delete: remove some old project

--- Vue/Vue3/hello-vue3/src/hooks/usePoint.ts
+++
@@ -1,9 +0,0 @@
-import { onMounted, inject } from 'vue'
-
-export function usePoint() {
-  const track = inject('track')
-  onMounted(() => {
-    (track as any).getPerformanceInfo()
-  })
-}
-
--- Vue/Vue3/hello-vue3/src/hooks/useRouterFrom.ts
+++
@@ -1,7 +0,0 @@
-import { onBeforeRouteLeave, useRoute } from "vue-router"
-import { useQueryStore } from "@/store/useQueryStore"
-export const useRouterFrom = () => {
-  const query = useQueryStore()
-  const route = useRoute()
-  console.log(route, query.from)
-}
